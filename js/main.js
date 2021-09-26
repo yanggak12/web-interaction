d2c13d37edf4b541522f6ba69492d26a12610d4d
양윤상 <yanggak12@naver.com>
2021-09-26 15:15:31 +0900

    현재 활성시킬 섹션 결정하기

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 (() => {
+  let yOffset = 0; // window.scrollY 대신 쓸 변수
+  let prevScrollHeight = 0; // 현재 스트롤 위치보다 이전에 위치한 스트롤 섹션들의 스크롤 높이 합
+  let currentScene = 0; // 현재 활성화된 스크롤 섹션
+
   // 이렇게 하는 이유. 지역변수로 선언하려고 함수를 바로 실행해버림.
   const sceneInfo = [
     {
@@ -47,6 +51,24 @@
       ].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
     }
   }
+  function scrollLoop() {
+    prevScrollHeight = 0;
+    for (let i = 0; i < currentScene; i++) {
+      prevScrollHeight += sceneInfo[i].scrollHeight;
+    }
+    if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+      currentScene++;
+    }
+    if (yOffset < prevScrollHeight) {
+      if (currentScene === 0) return; // 브라우저 바운스 효과 마이너스 되는 경우를 예방함
+      currentScene--;
+    }
+    console.log(currentScene);
+  }
   window.addEventListener("resize", setLayout);
+  window.addEventListener("scroll", () => {
+    yOffset = window.scrollY;
+    scrollLoop();
+  });
   setLayout();
 })();
